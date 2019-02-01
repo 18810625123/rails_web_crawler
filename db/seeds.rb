ad473ee488c6f36865818ff6f6fefd610f902e16
刘栋 <chuangye201012@163.com>
2019-02-01 12:21:09 +0800

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+unless Crawler::Menu.find_by_name 'Crawler::Menu'
+	Crawler::Menu.create name: '系统首页'
+end
